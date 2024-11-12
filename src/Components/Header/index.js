61cf17e1515992ff0280fc20f423d4804840bcf8
saagor16 <saagor.16@gmail.com>
2024-11-12 09:43:04 +0600

    logo create and add

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,3 +1,7 @@
+import { Link } from "react-router-dom";
+import Logo from "../../assets/images/logo.png"
+import Button from '@mui/material/Button'
+
 const Header = ()=>{
     return(
         <>
@@ -5,7 +9,20 @@ const Header = ()=>{
                 <div className="top-strip bg-blue">
                     <div className="container">
                     <p className="mb-0 mt-0 text-center">Due to the <b>COVID 19</b> epidemic, orders may be processed with a slight delay</p>
+                    </div>
+                </div>
 
+                <div className="header">
+                    <div className="container">
+                        <div className="row">
+                            <div className="logoWrapper d-flex align-items-center col-sm-3">
+                                <Link to={'/'}><img src={Logo} alt="Logo"/></Link>
+                            </div>
+
+                            <div className="part2 d-flex align-items-center col-sm-10">
+                                <Button className="countryDrop"></Button>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
