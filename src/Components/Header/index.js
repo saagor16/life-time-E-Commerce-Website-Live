a65cefb3e375cd3bbca0aff92eb3b16790b60ab8
saagor16 <saagor.16@gmail.com>
2024-11-12 12:12:11 +0600

    search butoon and bar

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,34 +1,52 @@
 import { Link } from "react-router-dom";
-import Logo from "../../assets/images/logo.png"
-
+import Logo from "../../assets/images/logo.png";
+import Button from "@mui/material/Button";
 import CountryDropdown from "../CountryDropdown";
+import { IoSearch } from "react-icons/io5";
 
-const Header = ()=>{
-    return(
-        <>
-            <div className="headerWrapper">
-                <div className="top-strip bg-blue">
-                    <div className="container">
-                    <p className="mb-0 mt-0 text-center">Due to the <b>COVID 19</b> epidemic, orders may be processed with a slight delay</p>
-                    </div>
+const Header = () => {
+  return (
+    <>
+      <div className="headerWrapper">
+        <div className="top-strip bg-blue">
+          <div className="container">
+            <p className="mb-0 mt-0 text-center">
+              Due to the <b>COVID 19</b> epidemic, orders may be processed with
+              a slight delay
+            </p>
+          </div>
+        </div>
+        {/* Main Header Section */}
+        <header className="header">
+          <div className="container">
+            <div className="row align-items-center">
+              {/* Logo */}
+              <div className="logoWrapper d-flex align-items-center col-sm-3">
+                <Link to={"/"}>
+                  <img src={Logo} alt="Logo" />
+                </Link>
+              </div>
+              {/* Country Dropdown and Search Bar */}
+              <div className="part2 d-flex align-items-center col-sm-9">
+                <CountryDropdown />
+
+                {/* Header Search Bar */}
+                <div className="headerSearch mr-3  ml-3">
+                  <input
+                    type="text"
+                    placeholder="Search for products..."
+                  />
+                  <Button>
+                    <IoSearch />
+                  </Button>
                 </div>
-
-                <div className="header">
-                    <div className="container">
-                        <div className="row">
-                            <div className="logoWrapper d-flex align-items-center col-sm-3">
-                                <Link to={'/'}><img src={Logo} alt="Logo"/></Link>
-                            </div>
-
-                            <div className="part2 d-flex align-items-center col-sm-10">
-                               <CountryDropdown></CountryDropdown>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+              </div>
             </div>
-        </>
-    )
-}
+          </div>
+        </header>
+      </div>
+    </>
+  );
+};
 
 export default Header;
