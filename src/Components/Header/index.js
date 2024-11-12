c2137f3c94582e089165e7256eb82a4dcde39abf
saagor16 <saagor.16@gmail.com>
2024-11-12 14:30:07 +0600

    header creat

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -7,56 +7,62 @@ import { FiUser } from "react-icons/fi";
 
 const Header = () => {
   return (
-    <>
-      <div className="headerWrapper">
-        <div className="top-strip bg-blue">
-          <div className="container">
-            <p className="mb-0 mt-0 text-center">
-              Due to the <b>COVID 19</b> epidemic, orders may be processed with
-              a slight delay
-            </p>
-          </div>
+    <div className="headerWrapper">
+      {/* Top Announcement Strip */}
+      <div className="top-strip bg-blue">
+        <div className="container">
+          <p className="mb-0 mt-0 text-center">
+            Due to the <b>COVID-19</b> epidemic, orders may be processed with
+            a slight delay
+          </p>
         </div>
-        {/* Main Header Section */}
-        <header className="header">
-          <div className="container">
-            <div className="row align-items-center">
-              {/* Logo */}
-              <div className="logoWrapper d-flex align-items-center col-sm-3">
-                <Link to={"/"}>
-                  <img src={Logo} alt="Logo" />
-                </Link>
+      </div>
+      
+      {/* Main Header Section */}
+      <header className="header">
+        <div className="container">
+          <div className="row align-items-center">
+            {/* Logo Section */}
+            <div className="logoWrapper d-flex align-items-center col-sm-3">
+              <Link to="/">
+                <img src={Logo} alt="Logo" />
+              </Link>
+            </div>
+            
+            {/* Country Dropdown, Search Bar, and User/Cart Section */}
+            <div className="part2 d-flex align-items-center col-sm-9">
+              <CountryDropdown />
+
+              {/* Header Search Bar */}
+              <div className="headerSearch mx-3">
+                <input type="text" placeholder="Search for products..." />
+                <Button>
+                  <IoSearch />
+                </Button>
               </div>
-              {/* Country Dropdown and Search Bar */}
-              <div className="part2 d-flex align-items-center col-sm-9">
-                <CountryDropdown />
 
-                {/* Header Search Bar */}
-                <div className="headerSearch mr-3  ml-3">
-                  <input
-                    type="text"
-                    placeholder="Search for products..."
-                  />
-                  <Button>
-                    <IoSearch />
-                  </Button>
+              {/* User and Cart Icons */}
+              <div className="part3 d-flex align-items-center ml-auto">
+                <Button className="circle mr-3">
+                  <FiUser />
+                </Button>
+                <div className="ml-auto d-flex cartTab align-items-center">
+                  <span className="price">$3.29</span>
+                  <div className="position-relative ml-2">
+                    <Button className="circle">
+                      <IoBagOutline />
+                    </Button>
+                    <span className="count d-flex align-items-center justify-content-center">
+                      1
+                    </span>
+                  </div>
                 </div>
-
-                {/* header search end bars */}
-                <div className="part3 d-flex align-items-center ml-auto">
-                    <Button className="circle mr-3"><FiUser></FiUser></Button>
-                    <div className="ml-auto d-flex cartTab">
-                        <span className="price">$3.29</span>
-                        <Button className="circle ml-2"><IoBagOutline></IoBagOutline></Button>
-                    </div>
-                </div>
-
               </div>
             </div>
           </div>
-        </header>
-      </div>
-    </>
+        </div>
+      </header>
+    </div>
   );
 };
 
