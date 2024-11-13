d2d925650b1bfbf6f2445ced3f7605c7dc09dcfe
saagor16 <saagor.16@gmail.com>
2024-11-13 15:36:13 +0600

    country add

--- src/Components/CountryDropdown/index.js
+++ src/Components/CountryDropdown/index.js
@@ -1,5 +1,8 @@
 import Button from "@mui/material/Button";
 import { FaAngleDown } from "react-icons/fa6";
+import Dialog from "@mui/material/Dialog";
+import { IoIosSearch } from "react-icons/io";
+
 const CountryDropdown = () => {
   return (
     <>
@@ -8,8 +11,21 @@ const CountryDropdown = () => {
           <span className="label">Your Location</span>
           <span className="name">Bangladesh</span>
         </div>
-        <span className="ml-auto"><FaAngleDown></FaAngleDown></span>
+        <span className="ml-auto">
+          <FaAngleDown></FaAngleDown>
+        </span>
       </Button>
+
+      <Dialog open={true} className="locationModel">
+        <h4>Choose your Delivery Location</h4>
+        <p>Enter your address and we will specify the offer for your area.</p>
+        <div className="headerSearch w-100">
+          <input type="text" placeholder="Search your area..." />
+          <Button>
+            <IoIosSearch />
+          </Button>
+        </div>
+      </Dialog>
     </>
   );
 };
