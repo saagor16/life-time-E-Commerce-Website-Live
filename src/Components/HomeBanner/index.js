47d66d7d1cd4b4447560aedcdaa50b7957fabf71
saagor16 <saagor.16@gmail.com>
2024-11-18 09:45:04 +0600

    Added four images to the banner section of the home page

--- src/Components/HomeBanner/index.js
+++ src/Components/HomeBanner/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/alt-text */
 import React from "react";
 import Slider from "react-slick";
 
@@ -15,7 +16,28 @@ const HomeBanner = () => {
       <div className="homeBannerSection">
         <Slider {...settings}>
           <div className="item">
-           <img src="" className="w-100"></img>
+            <img
+              src="https://sslimages.shoppersstop.com/sys-master/root/h1e/h9b/32374912352286/Static-Web---2024-04--01-new-hp-page.jpg"
+              className="w-100"
+            ></img>
+          </div>
+          <div className="item">
+            <img
+              src="https://sslimages.shoppersstop.com/sys-master/root/h98/h92/32015952117790/web_3093.jpg"
+              className="w-100"
+            ></img>
+          </div>
+          <div className="item">
+            <img
+              src="https://sslimages.shoppersstop.com/sys-master/root/h98/h92/32004480991262/And-Forever-New-web_901.jpg"
+              className="w-100"
+            ></img>
+          </div>
+          <div className="item">
+            <img
+              src="https://sslimages.shoppersstop.com/sys-master/root/hdd/h44/32004481122334/titan%2C-casio-web_31.jpg"
+              className="w-100"
+            ></img>
           </div>
         </Slider>
       </div>
