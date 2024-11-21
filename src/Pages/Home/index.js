f6cfe6c2381b74513ef673c250941096c1979867
saagor16 <saagor.16@gmail.com>
2024-11-21 23:31:14 +0600

    container use

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -4,6 +4,22 @@ const Home = () => {
   return (
     <>
       <HomeBanner></HomeBanner>
+
+      <section className="homeProducts">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-3">
+              <img
+                src="https://klbtheme.com/bacola/wp-contant/uploads/2021/04/banner-box.jpg"
+                className="cursor"
+                alt=""
+                />
+            </div>
+
+            <div className="col-md-3"></div>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
