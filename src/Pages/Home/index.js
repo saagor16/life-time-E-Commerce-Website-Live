8b359c44a1eba3863c77c79ebc709619b0f0d6b0
saagor16 <saagor.16@gmail.com>
2024-11-22 10:35:58 +0600

    Added Sidebanner section to the homepage

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -9,14 +9,25 @@ const Home = () => {
         <div className="container">
           <div className="row">
             <div className="col-md-3">
-              <img
-                src="https://klbtheme.com/bacola/wp-contant/uploads/2021/04/banner-box.jpg"
-                className="cursor"
-                alt=""
+              <div className="banner">
+                <img
+                  src="	https://res.cloudinary.com/dkgonwhvj/image/upload/v1731428306/1731428304529_New_Project_34.jpg"
+                  className="cursor"
+                  alt=""
                 />
+              </div>
             </div>
 
-            <div className="col-md-3"></div>
+            <div className="col-md-9 productRow">
+              <div className="d-flex align-item-center">
+                <div className="info w-75">
+                  <h3 className="mb-0 hd">BEST SELLERS </h3>
+                  <p className="text-light text-sml mb-0">
+                    Do not miss the current offers until the end of March.
+                  </p>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </section>
