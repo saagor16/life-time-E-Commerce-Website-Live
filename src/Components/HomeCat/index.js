3642ccfdd329b94c330665e12a6ef7161f2406d6
saagor16 <saagor.16@gmail.com>
2024-11-26 16:45:30 +0600

    Created slider for Featured Categories section

---
+++ src/Components/HomeCat/index.js
@@ -0,0 +1,168 @@
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { Navigation } from "swiper/modules";
+
+const HomeCat = () => {
+  return (
+    <>
+      <section className="homeCat">
+        <div className="container">
+          <Swiper
+            slidesPerView={10}
+            spaceBetween={20}
+            navigation={true}
+            pagination={{
+              clickable: true,
+              
+            }}
+            slidesPerGroup={1}
+            modules={[Navigation]}
+            className="mySwiper"
+          >
+            <SwiperSlide>
+              <div className="item text-center">
+                <img
+                  src="https:wp.alithemes.com/html/nest/demo/assets/imgs/shop/cat-9.png"
+                  alt=""
+                ></img>
+                <h6>Red Apple</h6>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="item text-center">
+                <img
+                  src="https:wp.alithemes.com/html/nest/demo/assets/imgs/shop/cat-9.png"
+                  alt=""
+                ></img>
+                <h6>Red Apple</h6>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="item text-center">
+                <img
+                  src="https:wp.alithemes.com/html/nest/demo/assets/imgs/shop/cat-9.png"
+                  alt=""
+                ></img>
+                <h6>Red Apple</h6>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="item text-center">
+                <img
+                  src="https:wp.alithemes.com/html/nest/demo/assets/imgs/shop/cat-9.png"
+                  alt=""
+                ></img>
+                <h6>Red Apple</h6>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="item text-center">
+                <img
+                  src="https:wp.alithemes.com/html/nest/demo/assets/imgs/shop/cat-9.png"
+                  alt=""
+                ></img>
+                <h6>Red Apple</h6>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="item text-center">
+                <img
+                  src="https:wp.alithemes.com/html/nest/demo/assets/imgs/shop/cat-9.png"
+                  alt=""
+                ></img>
+                <h6>Red Apple</h6>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="item text-center">
+                <img
+                  src="https:wp.alithemes.com/html/nest/demo/assets/imgs/shop/cat-9.png"
+                  alt=""
+                ></img>
+                <h6>Red Apple</h6>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="item text-center">
+                <img
+                  src="https:wp.alithemes.com/html/nest/demo/assets/imgs/shop/cat-9.png"
+                  alt=""
+                ></img>
+                <h6>Red Apple</h6>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="item text-center">
+                <img
+                  src="https:wp.alithemes.com/html/nest/demo/assets/imgs/shop/cat-9.png"
+                  alt=""
+                ></img>
+                <h6>Red Apple</h6>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="item text-center">
+                <img
+                  src="https:wp.alithemes.com/html/nest/demo/assets/imgs/shop/cat-9.png"
+                  alt=""
+                ></img>
+                <h6>Red Apple</h6>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="item text-center">
+                <img
+                  src="https:wp.alithemes.com/html/nest/demo/assets/imgs/shop/cat-9.png"
+                  alt=""
+                ></img>
+                <h6>Red Apple</h6>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="item text-center">
+                <img
+                  src="https:wp.alithemes.com/html/nest/demo/assets/imgs/shop/cat-9.png"
+                  alt=""
+                ></img>
+                <h6>Red Apple</h6>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="item text-center">
+                <img
+                  src="https:wp.alithemes.com/html/nest/demo/assets/imgs/shop/cat-9.png"
+                  alt=""
+                ></img>
+                <h6>Red Apple</h6>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="item text-center">
+                <img
+                  src="https:wp.alithemes.com/html/nest/demo/assets/imgs/shop/cat-9.png"
+                  alt=""
+                ></img>
+                <h6>Red Apple</h6>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="item text-center">
+                <img
+                  src="https:wp.alithemes.com/html/nest/demo/assets/imgs/shop/cat-9.png"
+                  alt=""
+                ></img>
+                <h6>Red Apple</h6>
+              </div>
+            </SwiperSlide>
+          </Swiper>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default HomeCat;
--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,29 +1,23 @@
 import Button from "@mui/material/Button";
 import HomeBanner from "../../Components/HomeBanner";
 import banner1 from "../../assets/images/banner1.jpg";
+import banner2 from "../../assets/images/banner2.jpg";
 import { IoIosArrowForward } from "react-icons/io";
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 import { Navigation } from "swiper/modules";
 import ProductItem from "../../Components/ProductItem";
+import HomeCat from "../../Components/HomeCat";
 
 const Home = () => {
-  var productSliderOptions = {
-    dots: true,
-    infinite: false,
-    speed: 500,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-  };
-
   return (
     <>
       <HomeBanner></HomeBanner>
+      <HomeCat></HomeCat>
 
       <section className="homeProducts">
         <div className="container">
@@ -31,6 +25,10 @@ const Home = () => {
             <div className="col-md-3">
               <div className="banner">
                 <img src={banner1} className="cursor w-100" alt="" />
+              </div>
+
+              <div className="banner mt-4">
+                <img src={banner2} className="cursor w-100" alt="" />
               </div>
             </div>
 
@@ -83,7 +81,6 @@ const Home = () => {
                   </SwiperSlide>
                 </Swiper>
               </div>
-
 
               <div className="d-flex align-item-center mt-5">
                 <div className="info w-75">
