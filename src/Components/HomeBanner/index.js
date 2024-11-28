d7ce4be3f4da556792fd7a0f86560fd244021049
saagor16 <saagor.16@gmail.com>
2024-11-28 16:56:15 +0600

    Started redesigning the slider for the homepage banner

--- src/Components/HomeBanner/index.js
+++ src/Components/HomeBanner/index.js
@@ -9,33 +9,35 @@ const HomeBanner = () => {
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
-    arrows:true,
-    autoplay:true
+    arrows: true,
+    autoplay: true,
   };
 
   return (
     <>
-      <div className="homeBannerSection">
-        <Slider {...settings}>
-          <div className="item">
-            <img
-              src="https://sslimages.shoppersstop.com/sys-master/root/h98/h92/32015952117790/web_3093.jpg"
-              className="w-100"
-            ></img>
-          </div>
-          <div className="item">
-            <img
-              src="https://sslimages.shoppersstop.com/sys-master/root/h98/h92/32004480991262/And-Forever-New-web_901.jpg"
-              className="w-100"
-            ></img>
-          </div>
-          <div className="item">
-            <img
-              src="https://sslimages.shoppersstop.com/sys-master/root/hdd/h44/32004481122334/titan%2C-casio-web_31.jpg"
-              className="w-100"
-            ></img>
-          </div>
-        </Slider>
+      <div className="container mt-3">
+        <div className="homeBannerSection">
+          <Slider {...settings}>
+            <div className="item">
+              <img
+                src="https://sslimages.shoppersstop.com/sys-master/root/h98/h92/32015952117790/web_3093.jpg"
+                className="w-100"
+              ></img>
+            </div>
+            <div className="item">
+              <img
+                src="https://sslimages.shoppersstop.com/sys-master/root/h98/h92/32004480991262/And-Forever-New-web_901.jpg"
+                className="w-100"
+              ></img>
+            </div>
+            <div className="item">
+              <img
+                src="https://sslimages.shoppersstop.com/sys-master/root/hdd/h44/32004481122334/titan%2C-casio-web_31.jpg"
+                className="w-100"
+              ></img>
+            </div>
+          </Slider>
+        </div>
       </div>
     </>
   );
