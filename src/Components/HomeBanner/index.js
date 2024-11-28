ae1def781daa12011741503fcba0d5423628075d
saagor16 <saagor.16@gmail.com>
2024-11-28 23:06:04 +0600

    Updated images for the homepage banner

--- src/Components/HomeBanner/index.js
+++ src/Components/HomeBanner/index.js
@@ -12,9 +12,9 @@ const HomeBanner = () => {
         <div className="homeBannerSection">
           <Swiper
             slidesPerView={1}
-            spaceBetween={0}
+            spaceBetween={15}
             navigation={true}
-            loop={true}
+            loop={false}
             autoplay={{
               delay: 2500,
               disableOnInteraction: false,
@@ -25,7 +25,7 @@ const HomeBanner = () => {
             <SwiperSlide>
               <div className="item">
                 <img
-                  src="https://sslimages.shoppersstop.com/sys-master/root/h98/h92/32015952117790/web_3093.jpg"
+                  src="https://res.cloudinary.com/dkgonwhvj/image/upload/v1731427456/1731427454225_1721277298204_banner.jpg"
                   className="w-100"
                 ></img>
               </div>
@@ -33,7 +33,7 @@ const HomeBanner = () => {
             <SwiperSlide>
               <div className="item">
                 <img
-                  src="https://sslimages.shoppersstop.com/sys-master/root/h98/h92/32015952117790/web_3093.jpg"
+                  src="https://res.cloudinary.com/dkgonwhvj/image/upload/v1731427470/1731427468095_New_Project_13.jpg"
                   className="w-100"
                 ></img>
               </div>
@@ -41,7 +41,7 @@ const HomeBanner = () => {
             <SwiperSlide>
               <div className="item">
                 <img
-                  src="https://sslimages.shoppersstop.com/sys-master/root/h98/h92/32015952117790/web_3093.jpg"
+                  src="https://res.cloudinary.com/dkgonwhvj/image/upload/v1731427534/1731427531889_New_Project_10.jpg"
                   className="w-100"
                 ></img>
               </div>
