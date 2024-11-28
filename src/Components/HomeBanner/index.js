96eb33ed783f844169390b37124e5e39ca11e3f1
saagor16 <saagor.16@gmail.com>
2024-11-28 22:58:07 +0600

    Updated the slider design for the homepage banner

--- src/Components/HomeBanner/index.js
+++ src/Components/HomeBanner/index.js
@@ -1,42 +1,52 @@
 /* eslint-disable jsx-a11y/alt-text */
 import React from "react";
-import Slider from "react-slick";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation,Autoplay } from "swiper/modules";
+import "swiper/css"; 
+import "swiper/css/navigation";
 
 const HomeBanner = () => {
-  var settings = {
-    dots: false,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: true,
-    autoplay: true,
-  };
-
   return (
     <>
       <div className="container mt-3">
         <div className="homeBannerSection">
-          <Slider {...settings}>
-            <div className="item">
-              <img
-                src="https://sslimages.shoppersstop.com/sys-master/root/h98/h92/32015952117790/web_3093.jpg"
-                className="w-100"
-              ></img>
-            </div>
-            <div className="item">
-              <img
-                src="https://sslimages.shoppersstop.com/sys-master/root/h98/h92/32004480991262/And-Forever-New-web_901.jpg"
-                className="w-100"
-              ></img>
-            </div>
-            <div className="item">
-              <img
-                src="https://sslimages.shoppersstop.com/sys-master/root/hdd/h44/32004481122334/titan%2C-casio-web_31.jpg"
-                className="w-100"
-              ></img>
-            </div>
-          </Slider>
+          <Swiper
+            slidesPerView={1}
+            spaceBetween={0}
+            navigation={true}
+            loop={true}
+            autoplay={{
+              delay: 2500,
+              disableOnInteraction: false,
+            }}
+            modules={[Navigation, Autoplay]}
+            className="mySwiper"
+          >
+            <SwiperSlide>
+              <div className="item">
+                <img
+                  src="https://sslimages.shoppersstop.com/sys-master/root/h98/h92/32015952117790/web_3093.jpg"
+                  className="w-100"
+                ></img>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="item">
+                <img
+                  src="https://sslimages.shoppersstop.com/sys-master/root/h98/h92/32015952117790/web_3093.jpg"
+                  className="w-100"
+                ></img>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="item">
+                <img
+                  src="https://sslimages.shoppersstop.com/sys-master/root/h98/h92/32015952117790/web_3093.jpg"
+                  className="w-100"
+                ></img>
+              </div>
+            </SwiperSlide>
+          </Swiper>
         </div>
       </div>
     </>
