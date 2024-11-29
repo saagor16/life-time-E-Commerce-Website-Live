30697e1ddf3291d64a5ddacd61dce23abcc4a700
saagor16 <saagor.16@gmail.com>
2024-11-29 15:25:31 +0600

    Added 'Add to Cart' button to the modal

--- src/Components/ProductModal/index.js
+++ src/Components/ProductModal/index.js
@@ -147,6 +147,15 @@ const ProductModal = (props) => {
               dummy text ever since the 1500s, when an unknown printer took a
               galley of type and scrambled it to make a type specimen book.
             </p>
+
+
+            <div className="d-flex align-items-center">
+              <div className="quantityDrop d-flex align-items-center">
+
+              </div>
+
+              <Button className="btn-blue btn-lg btn-big btn-round">Add to cart</Button>
+            </div>
           </div>
         </div>
       </Dialog>
