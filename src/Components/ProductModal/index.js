f392beb44c17c5a009122ff88aecb2d3674ded0c
saagor16 <saagor.16@gmail.com>
2024-11-29 10:07:12 +0600

    Set up width configuration for the modal

--- src/Components/ProductModal/index.js
+++ src/Components/ProductModal/index.js
@@ -16,15 +16,15 @@ const ProductModal = (props) => {
           <MdClose onClick={() => props.closeProductModal()}></MdClose>
         </Button>
 
-        <h4 className="mb-0">
+        <h4 className="mb-1 font-weight-bold">
           Alt Lit Smartwatch with Bluetooth Calling, 1.85 Inch TFT Screen
         </h4>
         <div className="d-flex align-items-center">
-          <div className="d-flex align-items-center">
+          <div className="d-flex align-items-center mr-4">
             <span>Brands:</span>
-            <span className="ml-2">Welch</span>
+            <span className="ml-2"><b>Welch</b></span>
           </div>
-          <Rating name="read-only" value={5} readOnly />
+          <Rating name="read-only" value={5} size="small" precision={0.5} readOnly />
 
         </div>
       </Dialog>
